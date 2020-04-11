ba61d782088b6174d599e477619a90253f4e0356
a.loginov <a.loginov@sakh.com>
2020-04-11 13:13:20 +1100

    Lesson 35. DI using Spring framework

--- src/main/java/com/example/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/com/example/sfgdi/controllers/PropertyInjectedController.java
@@ -1,9 +1,13 @@
 package com.example.sfgdi.controllers;
 
 import com.example.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting() {
