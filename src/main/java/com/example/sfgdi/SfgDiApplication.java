34b8b451cc98a5c7cfe7be4212a99faa0a47071a
a.loginov <a.loginov@sakh.com>
2020-04-11 15:39:28 +1100

    Lesson 37. Primary beans

--- src/main/java/com/example/sfgdi/SfgDiApplication.java
+++ src/main/java/com/example/sfgdi/SfgDiApplication.java
@@ -15,23 +15,20 @@ public class SfgDiApplication {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
 		MyController myController = (MyController)ctx.getBean("myController");
-		String greeting = myController.sayHello();
-		System.out.println(greeting);
+		System.out.println("------ Primary bean");
+		System.out.println(myController.sayHello());
 
 		System.out.println("------ Property");
-
 		PropertyInjectedController propertyInjectedController
 				= (PropertyInjectedController) ctx.getBean("propertyInjectedController");
 		System.out.println(propertyInjectedController.getGreeting());
 
 		System.out.println("------ Setter");
-
 		SetterInjectedController setterInjectedController
 				= (SetterInjectedController) ctx.getBean("setterInjectedController");
 		System.out.println(setterInjectedController.getGreeting());
 
 		System.out.println("------ Constructor");
-
 		ConstructorInjectedController constructorInjectedController
 				= (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 		System.out.println(setterInjectedController.getGreeting());
